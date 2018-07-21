05efe7f9a8b2f6472bd50c6340f97c598f29a279
Queco SM <sersalazar.marioni@gmail.com>
2018-07-21 21:18:53 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,11 @@
 # code your #position_taken? method here!
 board = [" "," "," "," "," "," "," "," "," "]
 def position_taken? (array, index)
-if array[index] == " "
+if array[index] != " "
 
 elsif array[index] != ""
 
-elsif array[index] == nil
+elsif array[index] != nil
 
 elsif array[index] == "X" || "O"
 end
