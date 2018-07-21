3c828654e1381b080a962e6c80d9c5b576cbe842
Queco SM <sersalazar.marioni@gmail.com>
2018-07-21 21:22:32 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+board = [" "," "," "," "," "," "," "," "," "]
+def position_taken? (array, index)
+array[index] != " "
+
+array[index] != ""
+
+array[index] == "X" || array[index] == "O"
+end
