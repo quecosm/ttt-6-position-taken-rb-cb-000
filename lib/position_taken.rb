1dbf249c956686b35668deb9294740512bb7736c
Queco SM <sersalazar.marioni@gmail.com>
2018-07-21 21:15:28 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 board = [" "," "," "," "," "," "," "," "," "]
 def position_taken? (array, index)
-array[index] != " "
-array[index] != ""
+array[index] != " " || array[index] != "" || array[index] != nil
+    
 end
